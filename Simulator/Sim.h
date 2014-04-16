4000b7f08d53f8bec2e63635297630f44af8e005
bondue <orochiSWE@gmail.com>
2014-04-16 10:39:36 +0200

    Comitting for a pull...

--- Simulator/Sim.h
+++ Simulator/Sim.h
@@ -4,11 +4,12 @@
  *  Created on: Apr 16, 2014
  *      Author: cawl-server
  */
-
+#include <sys/socket.h>
 #ifndef SIM_H_
 #define SIM_H_
 
 class Sim {
+	struct socket_in socket_out;
 public:
 	Sim();
 	virtual ~Sim();
