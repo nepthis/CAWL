d4131609448efc74029ae47b77ebec18e1038324
hakantheren <hakan.theren@gmail.com>
2014-05-07 13:00:46 +0200

    forgot CawlSocket.cpp

---
+++ Net/CawlSocket.cpp
@@ -0,0 +1,40 @@
+/*
+ * CawlSocket.cpp
+ *
+ *  Created on: May 7, 2014
+ *      Author: cawl-mobile
+ */
+
+#include "CawlSocket.h"
+
+namespace Net {
+
+CawlSocket::CawlSocket() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CawlSocket::~CawlSocket() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Net */
+
+CawlSocket::CawlSocket() {
+
+}
+
+int CawlSocket::send(Packets::CawlPacket *p) {
+	return 0;
+}
+
+int CawlSocket::rec(Packets::CawlPacket *p) {
+	return 0;
+}
+
+int CawlSocket::bind(Host *h) {
+	return 0;
+}
+
+CawlSocket::~CawlSocket() {
+}
