811cdb6a95d23eb74b3d406c060e39c3aa54dad0
hakantheren <hakan.theren@gmail.com>
2014-09-24 14:55:57 +0200

    IMUManager

--- IMU/IMUManager.cpp
+++ IMU/IMUManager.cpp
@@ -6,12 +6,56 @@
  */
 #include "IMUManager.h"
 
+std::mutex data_lock;
+
 IMU::IMUManager::IMUManager() {
+	filter = false;
+	conn = false;
+	offset_accx = 0;
+	offset_accy = 0;
+	offset_accz = 0;
+	bufsize = 0;
+	devid = -1;
+	old.accx = 0;
+	old.accy = 0;
+	old.accz = 0;
+	old.gyrox = 0;
+	old.gyroy = 0;
+	old.gyroz = 0;
+
+	init();
 }
 
-IMU::IMUManager::~IMUManager() {
-}
 
+/*
+ * Start the interfacing the IMU
+ */
+int IMU::IMUManager::init() {
+	while(devid<0){
+		devid = getDev();
+		if(devid == -1){sleep(5);}
+
+	}
+
+	if(RS232_OpenComport(devid, BAUD)) {
+		return 1;
+	}else{
+		conn = true;
+	}
+
+	std::thread t1 (&IMU::IMUManager::readImu, this);
+	std::thread t2 (&IMU::IMUManager::getControl , this);
+
+	t1.detach();
+	t2.detach();
+
+	//Todo Start threads for reading the comport
+	while(1){
+		//printf("%s\n",(char*)buf);
+		sleep(1);
+	}
+	return -1;
+}
 
 /*
  * Used to see what IMU if any and to recieve it's
@@ -22,72 +66,211 @@ IMU::IMUManager::~IMUManager() {
  *
  */
 int IMU::IMUManager::getDev() {
+
 	DIR           *d;
 	struct dirent *dir;
 
-	std::string full_device_name = NULL;
-	std::string dev;
-	char* dev_path = "/dev/serial/by-id/";
-
+	std::string full_device_name = "";
+	char* dev;
+	char* dev_path = (char*)"/dev/serial/by-id/";
 	d = opendir(dev_path);
-	if(d){
-		while((dir = readdir(d)) != NULL){
+
+	if(d != NULL){
+		dir = readdir(d);
+		while(dir != NULL){
 			dev = dir->d_name;
+			dir = readdir(d);
+
 			for(int i = 0; i != dev_id.size(); ++i){
-				if(dev.find(std::get<0>(dev_id[i]))){
-					full_device_name = dev;
+				std::string tmp = std::get<0>(dev_id[i]);
+				std::string devtmp(dev);
+				std::size_t f = devtmp.find(tmp);
+				if(static_cast<int>(f) != -1){
+					full_device_name = devtmp;
 					calibrate(
 							std::get<1>(dev_id[i]),
 							std::get<2>(dev_id[i]),
 							std::get<3>(dev_id[i]));
-				}else{
-					return 1;
 				}
 			}
 		}
+		closedir(d);
 		std::stringstream ss;
-		ss << dev_path << full_device_name;
-		ss >> dev_path;
-		return devnr.find(realpath(dev_path,NULL))->first;
+		ss << dev_path << full_device_name.c_str();
+		ss >> full_device_name;
+
+		return devnr.find(realpath(full_device_name.c_str(),NULL))->second;
 	}else{
-		return 1;
+		closedir(d);
+		return -1;
 	}
 
 }
+
+
 
 
 /*
- * Start the interfacing the IMU
+ * ?
  */
-int IMU::IMUManager::init() {
-	while(devid<=0){
-		devid = getDev();
-		sleep(IMU_timeout);
-	}
-	return -1;
-
+void IMU::IMUManager::calibrate(int x, int y , int z) {
+	offset_accx = x;
+	offset_accy = y;
+	offset_accz = z;
 }
 
-int IMU::IMUManager::calibrate(int x, int y , int z) {
-
-	return -1;
-}
-
-IMU::imud IMU::IMUManager::readData() {
-	return imudata;
-}
-
-void IMU::IMUManager::setFilter(bool filterOn) {
-	filter = filterOn;
-}
-
+/*
+ *?
+ */
 void IMU::IMUManager::readImu() {
+	int n,t=0;
+	unsigned char b;
+	unsigned char buf[22];
+	unsigned char buftemp[22];
+
+	while(1){
+		usleep(IMU_TIMEOUT);
+		for(int i = 0; i < 5;++i){
+			memset(&buf,0,22);
+			n = RS232_PollComport(devid, buf, 1);
+			if(n!=0 && buf[0]!=0x00){
+				i = 0;
+			}
+			if(n==0 && i>0){
+				i--;
+				usleep(IMU_TIMEOUT);
+			}
+
+		}
+		memset(&buf,0,22);
+
+		while(1){
+			n = RS232_PollComport(devid, buf+t, 22-t);
+			t=t+n;
+			uint32_t gyrox = 0;
+
+			/* Spår utskrift för bitrepresentaiton av kommandon
+			const char* beg = reinterpret_cast<const char*>(&buf);
+			const char* end = beg + sizeof(buf);
+			while(beg != end){
+				std::cout << std::bitset<CHAR_BIT>(*beg++);
+			}std::cout << std::endl;
+			 */
+			if(t==22 && memcmp((char*)buf+18,(char*)"\x00\x00\x00\x00",4)==0){
+				t = n = 0;
+				setData((char*)buf);
+				memset(&buf,0,22);
+			}else if(t>22){
+				t = n = 0;
+				memset(&buf,0,22);
+				break;
+			}else if(t==22 && memcmp((char*)buf+18,(char*)"\x00\x00\x00\x00",4)!=0){
+				t = n = 0;
+				memset(&buf,0,22);
+				break;
+			}
+			usleep(IMU_TIMEOUT);
+		}
+	}
 }
 
-IMU::imud IMU::IMUManager::filterData(imud rawData) {
 
-	return rawData;
+void IMU::IMUManager::setData(char* command) {
+
+	//Set gyrox,y,z from command (Gyros)
+	memmove(&gytemp,command,4);
+	gytemp = __builtin_bswap32(gytemp);
+	gytemp = (gytemp & 0x3FFFC00) >> 10;
+	gyrox = 0xffff&gytemp;
+
+	memmove(&gytemp,command+4,4);
+	gytemp = __builtin_bswap32(gytemp);
+	gytemp = (gytemp & 0x3FFFC00) >> 10;
+	gyroy = 0xffff&gytemp;
+
+	memmove(&gytemp,command+8,4);
+	gytemp = __builtin_bswap32(gytemp);
+	gytemp = (gytemp & 0x3FFFC00) >> 10;
+	gyroz = 0xffff&gytemp;
+
+	//Set accx,y,z from  command (accelerometers) 9 bit resolution
+	memmove(&actemp,command+12,2);
+	actemp = __builtin_bswap16(actemp);
+	accx = (actemp - getX()) >> 1;
+
+	memmove(&actemp,command+14,2);
+	actemp = __builtin_bswap16(actemp);
+	accy = (actemp - getY()) >> 1;
+
+	memmove(&actemp,command+16,2);
+	actemp = __builtin_bswap16(actemp);
+	accz = (actemp - getZ()) >> 1;
+
+
+	data_lock.lock();
+	imudata.gyrox = gyroToFloat(gyrox);
+	imudata.gyroy = gyroToFloat(gyroy);//*NEG_GYRO_Y; //Inverted placement on board
+	imudata.gyroz = gyroToFloat(gyroz);
+	imudata.accx  = accToFloat(accx);
+	imudata.accy  = accToFloat(accy);
+	imudata.accz  = accToFloat(accz);
+	filterData();
+	data_lock.unlock();
 }
 
-void IMU::IMUManager::setData(imud filteredData) {
+/*
+ * ?
+ */
+Packets::ImuPack IMU::IMUManager::getControl() {
+
+	data_lock.lock();
+	float accx = imudata.accx;
+	float accy = imudata.accy;
+	float accz = imudata.accz;
+	float gyrox= imudata.gyrox;
+	float gyroy= imudata.gyroy;
+	float gyroz= imudata.gyroz;
+	data_lock.unlock();
+
+	Packets::ImuPack p = Packets::ImuPack();
+	p.setSensorDataValue(GYRO_X,gyrox);
+	p.setSensorDataValue(GYRO_Y,gyroy);
+	p.setSensorDataValue(GYRO_Z,gyroz);
+	p.setSensorDataValue(ACC_X,accx);
+	p.setSensorDataValue(ACC_Y,accy);
+	p.setSensorDataValue(ACC_Z,accz);
+	p.stampTime();
+
+	return p;
+
+	//Spårutskrifter
+	//std::cout << "X: "<< accx << " Y: " << accy << " Z: " << accz << std::endl;
+	//std::cout << "X: "<<gyrox << " Y: " <<gyroy << " Z: " <<gyroz << std::endl;
+}
+
+void IMU::IMUManager::filterData() {
+
+	float accx = (imudata.accx*FILTER_RATIO)+(old.accx*(1-FILTER_RATIO));
+	float accy = (imudata.accy*FILTER_RATIO)+(old.accy*(1-FILTER_RATIO));
+	float accz = (imudata.accz*FILTER_RATIO)+(old.accz*(1-FILTER_RATIO));
+	float gyrox= (imudata.gyrox*(1-FILTER_RATIO))+(old.gyrox*(FILTER_RATIO));
+	float gyroy= (imudata.gyroy*(1-FILTER_RATIO))+(old.gyroy*(FILTER_RATIO));
+	float gyroz= (imudata.gyroz*(1-FILTER_RATIO))+(old.gyroz*(FILTER_RATIO));
+
+	imudata.accx = accx;
+	imudata.accy = accy;
+	imudata.accz = accz;
+	imudata.gyrox= gyrox;
+	imudata.gyroy= gyroy;
+	imudata.gyroz= gyroz;
+
+	old.accx = imudata.accx;
+	old.accy = imudata.accy;
+	old.accz = imudata.accz;
+	old.gyrox =imudata.gyrox;
+	old.gyroy =imudata.gyroy;
+	old.gyroz =imudata.gyroz;
+}
+
+IMU::IMUManager::~IMUManager() {
 }
--- IMU/IMUManager.h
+++ IMU/IMUManager.h
@@ -8,21 +8,38 @@
 #ifndef IMUMANAGER_H_
 #define IMUMANAGER_H_
 
-
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h> //Used for printf
 #include <dirent.h>
 #include <string>
 #include <vector>
 #include <tuple>
 #include <map>
+#include <mutex>
+#include <thread>
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
 #include "rs232.h"
-
+#include "../Packets/ImuPack.h"
 
 #define BAUD 500000
 #define PATH "/dev/serial/by-id/"
+#define BUFFER_SIZE 1024
+#define IMU_TIMEOUT 500
+#define FILTER_RATIO 0.1
+#define CALIBRATION_N 100
 
+//ACC PARAMS
+#define MAX_VOLTAGE 3.3
+#define SENSITIVITY 0.192
+#define RESOLUTION_ACC 512
+
+//GYRO PARAMS
+#define M_PI 3.14159265358979323846
+#define GYRO_SCALE 80
+#define NEG_GYRO_Y -1
 
 namespace IMU{
 
@@ -40,22 +57,38 @@ class IMUManager{
 public:
 	IMUManager();
 	virtual ~IMUManager();
-
-	int init();
-	imud readData();
-	void setFilter(bool);
+	Packets::ImuPack getControl();
+	bool connected(){return conn;}
+	bool 			conn;
 
 private:
-	int devid;
-	int offset_gyrox;
-	int offset_gyroy;
-	int offset_gyroz;
-	int offset_accx;
-	int offset_accy;
-	int offset_accz;
-	bool filter;
-	imud imudata;
+	int 			devid;
+	int 			offset_accx;
+	int 			offset_accy;
+	int 			offset_accz;
 
+	int16_t 		gyrox;
+	int16_t 		gyroy;
+	int16_t 		gyroz;
+	int16_t 		accx;
+	int16_t 		accy;
+	int16_t 		accz;
+
+	uint32_t 		gytemp;
+	int16_t 		actemp;
+
+	bool 			filter;
+
+	imud 			imudata;
+	imud 			old;
+
+	unsigned char 	buf[BUFFER_SIZE];
+	int 			bufsize;
+
+	DIR           	*d;
+	struct dirent 	*dir;
+	std::string   	full_device_name;
+	std::string 	dev;
 
 	/* Vector containing id and its measured offset for accl.
 	 * in the order:
@@ -63,10 +96,11 @@ private:
 	 */
 	std::vector<std::tuple<std::string,int,int,int,std::string>> dev_id = {
 			std::make_tuple("A4001kB2",509,511,509,"IMU1"),
-			std::make_tuple("AD01REIF",510,505,529,"IMU2")};
+			std::make_tuple("AD01REIF",509,505,532,"IMU2")};
 
 	/* Used to map IMU to comport with rs232. To add more
-	/interfaces fill map as is in rs232.c
+	/interfaces fill map as is in rs232.c, for the moment
+	 * only USB.
 	 */
 	std::map<std::string,int> devnr = {
 			{"/dev/ttyUSB0",16},
@@ -76,19 +110,19 @@ private:
 			{"/dev/ttyUSB4",20},
 			{"/dev/ttyUSB5",21},};
 
-	//Timeout for serialinterface/IMU
-	int IMU_timeout = 10;
-
-	DIR           *d;
-	struct dirent *dir;
-	std::string full_device_name;
-	std::string dev;
-
-	int calibrate(int, int, int);
-	void readImu();
-	imud filterData(imud rawData);
-	void setData(imud filteredData);
+	int init();
 	int getDev();
+	int getX(){return offset_accx;}
+	int getY(){return offset_accy;}
+	int getZ(){return offset_accz;}
+
+	void calibrate(int x, int y , int z);
+	void readImu();
+	void filterData();
+	void setData(char*);
+
+	float gyroToFloat(int value){return (value/GYRO_SCALE)*(M_PI/180);}
+	float accToFloat(int value){return value*(MAX_VOLTAGE/SENSITIVITY)/(RESOLUTION_ACC-1);}
 };
 }
 
